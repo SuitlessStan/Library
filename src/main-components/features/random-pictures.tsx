5c8772e84ff4b109e3a47966e6ddfd2ecb92004a
Esam <lands.of.48@gmail.com>
2021-11-18 03:39:22 +0200

    trying to add the unsplash api

--- src/main-components/features/random-pictures.tsx
+++ src/main-components/features/random-pictures.tsx
@@ -1,26 +1,38 @@
 import * as React from 'react';
 import axios from 'axios';
-import {Box,Image} from '@chakra-ui/react';
+import { Box, Image,CircularProgress, Code, Link } from '@chakra-ui/react';
+import { createApi } from "unsplash-js";
 
-const URL:string = 'https://source.unsplash.com/random';
+const URL: string = 'https://source.unsplash.com/random';
+
+type Photo = {
+  id: number;
+  width: number;
+  height: number;
+  urls: { large: string; regular: string; raw: string; small: string };
+  color: string | null;
+  user: {
+    username: string;
+    name: string;
+  };
+};
+
+const api = createApi({
+  accessKey: "ydQzHFVBUsrPJEG7fZohNC7_NIN2pwYS_ql6RnsRd7c"
+});
 
 
-interface RandomPictures {
-    src:string,
+const PhotoComp = (photo:Photo) => {
+  const {urls,width,height,id,user} = photo;
+  return (
+    <Box>
+      <Image src={urls.regular} alt={id.toString()} />
+      <Code textAlign="center">
+        Photograph by <Link href={`https://unsplash.com/@${user.username}`} isExternal>
+        `${user.username}`
+        </Link>
+      </Code>
+    </Box>
+  );
 }
 
-const randomPictures = (props:RandomPictures) => {
-    return (
-        <Box boxSize="md">
-            <Image src={props.src} alt="random picture" width={"100%"}/>
-        </Box>
-    );
-}
-
-function RandomPicture() {
-    return (
-        randomPictures({src:URL})
-    );
-}
-
-export default RandomPicture;
